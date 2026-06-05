ce7253ea0e2da5604550798cac626c83d2b0231a
linen101 <linen@protonmail.com>
2026-06-05 16:51:24 +0200

    corrected input integrity

--- Programs/Source/bench_fhe.py
+++ Programs/Source/bench_fhe.py
@@ -1,4 +1,19 @@
+# number of labels, needed for the computation of gini
+# t = 2
+# t = 3
+# t = 7
+# t = 10
 
+# number of elements in each vector\
+    # this captures the different combinations of attributes and attribute values 
+    # considered for possible split points.
+# ma = \alpha * \ceil[\sqrt(m)]    
+# ma = 15             # t = 3
+# ma = 44             # t = 3
+# ma = 108            # t = 2
+# ma = 136            # t = 10
+# ma = 202            # t = 2
+# ma = 2048           # t = 7
 import itertools
 import random
 import math
@@ -11,17 +26,19 @@ from Compiler import SC_fun
 program.set_security(40)
 
 # set the bit length of the cleartext for the comparisons
-program.set_bit_length(64)
+program.set_bit_length(32)
 
 # Set the bit length based on edabit
 bit_length = program.bit_length
 print_ln("%s-bit_length", bit_length)
 
-#samples
-samples = 100
+#default parameters for the benchmarks, which can be overwritten at compile time by passing arguments to the compiler
+n = 100 # number of samples
+m = 9   # number of attributes, needed for the computation of the number of possible split points
+alpha = 4 # number of possible attribute values, needed for the computation of the number of possible split points
+t = 3   # number of labels
+d = 4   # depth of the tree
 
-# depth of the tree
-d=4
 # argmax to be computed collaboratively between the servers
 # in parallel, on each tree level 
 # e.g. if we assume 10 servers, each one training 10 decision trees\
@@ -34,27 +51,8 @@ d4 = 800
 d5 = 0
 d6 = 0
 h = 800
+n_threads = 4
 
-n_threads = 48
-
-# number of labels, needed for the computation of gini
-#t = 2
-t = 3
-#t = 7
-#t = 10
-
-# number of elements in each vector\
-    # this captures the different combinations of attributes and attribute values 
-    # considered for possible split points.
-# n = \alpha * \ceil[\sqrt(m)]    
-n = 15              # t = 3
-#n = 44             # t = 3
-#n = 108            # t = 2
-#n = 136            # t = 10
-#n = 202            # t = 2
-#n = 2048           # t = 7
-
-x=12600
 # n = 8 * \ceil[\sqrt(m)] 
 
 try:
@@ -63,40 +61,35 @@ except:
     pass
 
 try:
-    t = int(program.args[2])
+    m = int(program.args[2])
 except:
     pass
 
 try:
-    d = int(program.args[3])
+    alpha = int(program.args[3])
 except:
     pass
 
 try:
-    samples = int(program.args[4])
-except:
-    pass
-
-print_ln("Compiled with n=%s, t=%s, d=%s", n, t, d)
-
-"""
-try:
-    x = int(program.args[1])
+    t = int(program.args[4])
 except:
     pass
 
 try:
-    d = int(program.args[2])
+    d = int(program.args[5])
 except:
     pass
 
-print_ln("Compiled with x=%s, d=%s", x, d)
-"""
+ma = alpha * math.ceil(math.sqrt(m)) # number of elements in each vector, which captures the different combinations of attributes and attribute values considered for possible split points. This is needed for the computation of the gini index, as well as for the argmax.
+
+print_ln("Compiled with n=%s, m=%s, alpha=%s, t=%s, d=%s", n, m, alpha, t, d)
+
 
 if (d==5):
     d5 = 1600
     h = 1600
 if (d==6):
+    d5 = 1600
     d6 = 3100
     h = 3100
 
@@ -112,12 +105,12 @@ def compute_gini(a,b,c,d,t):
     return gini
     
 def create_a_b():
-    a = types.sint.get_random_int(32, size=1)
-    b = types.sint.get_random_int(32, size=1)
-    return (a,b)
+    a = types.sint.get_random_int(32)
+    b = types.sint.get_random_int(32)
+    return (MemValue(a),MemValue(b))
 
 def create_val():
-    val = types.sint.get_random_int(32, size=1)
+    val = types.sint.get_random_int(32)
     val = val.square()
     return  MemValue(val)
 
@@ -144,18 +137,23 @@ def bench_max(a_values):
     idx = a_max.reveal()   
     return a_values[idx].reveal()
 
-a_array = sint.Array(n)
-a_values = create_a_values(n)  
+
+a_array = sint.Array(ma)
+a_values = create_a_values(ma)  
 a_array.assign(a_values)
 
+t_array = sint.Array(t)
+t_values = create_a_values(t)  
+t_array.assign(t_values)
 
 # benchmark the argmax 
 start_timer(1)
+
 @for_range_opt_multithread(n_threads, d1)
 def _(i):
     #@for_range(l)
     #def _(i):
-    a_max = bench_argmax(a_array)
+    a_max = ml.argmax(a_array)
 @for_range_opt_multithread(n_threads, d2)
 def _(i):
     #@for_range(l)
@@ -170,93 +168,58 @@ def _(i):
 def _(i):
     #@for_range(l)
     #def _(i):
-    a_max = bench_argmax(a_array)  
+    if (d5==0):
+        a_max = bench_argmax(t_array)
+    if (d5!=0):
+        a_max = bench_argmax(a_array)      
 @for_range_opt_multithread(n_threads, d5)
 def _(i):
     #@for_range(l)
     #def _(i):
-    a_max = bench_argmax(a_array) 
+    if (d6==0):
+        a_max = bench_argmax(t_array)
+    if (d6!=0):
+        a_max = bench_argmax(a_array)
 @for_range_opt_multithread(n_threads, d6)
 def _(i):
     #@for_range(l)
     #def _(i):
-    a_max = bench_argmax(a_array)               
+    a_max = bench_argmax(t_array)               
 stop_timer(1)
 
 # benchmark computation of GINI index with G' formula of overleaf
 #(after FHE preprocessing: n as above )
 #(without FHE, TOTAL IN MPC, n as below) 
-a = create_val()
-b = create_val()
-c = create_val()
-d = create_val()
+aval = create_val()
+bval = create_val()
+cval = create_val()
+dval = create_val()
 start_timer(2)
-@for_range_opt_multithread(n_threads, d1*n)
+@for_range_opt_multithread(n_threads, d1*ma)
 def _(i):
-    compute_gini(a,b,c,d,t)     
-@for_range_opt_multithread(n_threads, d2*n)
+    compute_gini(aval,bval,cval,dval,t)     
+@for_range_opt_multithread(n_threads, d2*ma)
 def _(i):
-    compute_gini(a,b,c,d,t)
-@for_range_opt_multithread(n_threads, d3*n)
+    compute_gini(aval,bval,cval,dval,t)
+@for_range_opt_multithread(n_threads, d3*ma)
 def _(i):
-    compute_gini(a,b,c,d,t)
-@for_range_opt_multithread(n_threads, d4*n)
+    compute_gini(aval,bval,cval,dval,t)
+@for_range_opt_multithread(n_threads, d4*ma)
 def _(i):
-    compute_gini(a,b,c,d,t)
-@for_range_opt_multithread(n_threads, d5*n)
+    compute_gini(aval,bval,cval,dval,t)
+@for_range_opt_multithread(n_threads, d5*ma)
 def _(i):
-    compute_gini(a,b,c,d,t)
-@for_range_opt_multithread(n_threads, d6*n)
+    compute_gini(aval,bval,cval,dval,t)
+@for_range_opt_multithread(n_threads, d6*ma)
 def _(i):
-    compute_gini(a,b,c,d,t)        
+    compute_gini(aval,bval,cval,dval,t)        
 stop_timer(2)
 
-
-a_array = sint.Array(t)
-a_values = create_a_values(t)  
-a_array.assign(a_values)
-#label for the leaves
-start_timer(4)
-# benchmark the argmax 
-start_timer(1)
-@for_range_opt_multithread(n_threads, d1)
-def _(i):
-    #@for_range(l)
-    #def _(i):
-    a_max = bench_argmax(a_array)
-@for_range_opt_multithread(n_threads, d2)
-def _(i):
-    #@for_range(l)
-    #def _(i):
-    a_max = bench_argmax(a_array)
-@for_range_opt_multithread(n_threads, d3)
-def _(i):
-    #@for_range(l)
-    #def _(i):
-    a_max = bench_argmax(a_array)
-@for_range_opt_multithread(n_threads, d4)
-def _(i):
-    #@for_range(l)
-    #def _(i):
-    a_max = bench_argmax(a_array)  
-@for_range_opt_multithread(n_threads, d5)
-def _(i):
-    #@for_range(l)
-    #def _(i):
-    a_max = bench_argmax(a_array) 
-@for_range_opt_multithread(n_threads, d6)
-def _(i):
-    #@for_range(l)
-    #def _(i):
-    a_max = bench_argmax(a_array) 
-stop_timer(4)    
-
-a = create_val()
-b = create_val()
-T=h*100*(2*samples*n*t) # number of multiplications in the whole tree, for the given parameters, as a function of n, t, d, and samples. This is used for benchmarking the input integrity check.
+(x,y) = create_a_b()
+tt = d*n*m*alpha + d*(n*m + t)*(alpha + 1) + d*n*m*t # number of multiplications in the whole tree, for the given parameters, as a function of n, t, d, and samples. This is used for benchmarking the input integrity check.
 #input integrity
-start_timer(5)
-@for_range_opt_multithread(n_threads, T)
+start_timer(4)
+@for_range_opt_multithread(n_threads, tt)
 def _(i):
-    c = a*b
-stop_timer(5)
+    z = x*y
+stop_timer(4)
